1e78eadd04bfad6439774a24cdaca5c0f56a43b3
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-07-21 23:47:49 -0700

    Work on impl

--- src/plinko.java
+++ src/plinko.java
@@ -4,7 +4,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.Scanner;
+import java.util.*;
 
 public class plinko {
     public static void main(String[] args) throws FileNotFoundException {
@@ -14,40 +14,108 @@ public class plinko {
         Scanner scanner = new Scanner(inputFile);
         PrintWriter printWriter = new PrintWriter(outputFile);
 
-        String line;
-        String[] splitLine;
+        String nextLine;
+        String[] nextLineSplit;
+        int currentBoard = 0;
         while (scanner.hasNextLine()) {
-            line = scanner.nextLine();
-            if (line.equals("0")) {
+            nextLine = scanner.nextLine();
+            if (nextLine.equals("0")) {
+                System.out.println("DONE");
                 break;
             }
 
-            int levels = Integer.valueOf(line);
+            currentBoard++;
+            System.out.println("Board: " + currentBoard);
 
-            for (int i = 0; i < levels; i++) {
-                line = scanner.nextLine();
-                splitLine = line.split(" ");
-                int[] lineInts = new int[splitLine.length];
-                for (int x = 0; x < splitLine.length; x++) {
-                    lineInts[x] = Integer.valueOf(splitLine[x]);
+            int numberOfLevels = Integer.valueOf(nextLine);
+
+            Node head = null;
+            Queue<Node> parents = new LinkedList<>();
+
+            for (int currentLevel = 0; currentLevel < numberOfLevels; currentLevel++) {
+                nextLine = scanner.nextLine();
+
+                int[] pegsInLevel = Arrays.stream(nextLine.split(" "))
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
+
+                for (int peg = 0; peg < pegsInLevel.length; peg++) {
+                    Node node = new Node(pegsInLevel[peg]);
+
+                    if (head == null) {
+                        head = node;
+                    } else {
+                        if (peg != 0) {
+                            node.lparent = parents.poll();
+                            if (node.lparent != null) {
+                                node.lparent.r = node;
+                            }
+                        }
+
+                        if (peg != pegsInLevel.length - 1) {
+                            node.rparent = parents.poll();
+                            if (node.rparent != null) {
+                                node.rparent.l = node;
+                            }
+                        }
+                    }
+
+                    parents.add(node);
+                    parents.add(node);
                 }
-
-                
             }
+
+            System.out.println(getBestPath(head));
+            printWriter.println(getBestPath(head));
+
+            printWriter.close();
+
         }
+    }
+
+    private static int getBestPath(Node n) {
+        if (n.l == null && n.r == null) {
+            return n.value;
+        }
+
+        int l = getBestPath(n.l);
+        int r = getBestPath(n.r);
+
+        return l > r ? n.value + l : n.value + r;
     }
 
     public static class Node {
         int value;
         Node l;
         Node r;
-        Node parent;
+        Node lparent;
+        Node rparent;
 
-        Node(int value, Node parent) {
+        Node(int value) {
             this.value = value;
-            this.parent = parent;
+            this.lparent = null;
+            this.rparent = null;
             this.l = null;
             this.r = null;
+        }
+
+        public void print() {
+            print("", true);
+        }
+
+        // https://stackoverflow.com/questions/4965335/how-to-print-binary-tree-diagram
+        private void print(String prefix, boolean isTail) {
+            LinkedList<Node> children = new LinkedList<>();
+            if (l != null) children.add(l);
+            if (r != null) children.add(r);
+            System.out.println(prefix + (isTail ? "└── " : "├── ") + value);
+            for (int i = 0; i < children.size() - 1; i++) {
+                children.get(i).print(prefix + (isTail ? "    " : "│   "), false);
+            }
+            if (children.size() > 0) {
+                children.get(children.size() - 1)
+                        .print(prefix + (isTail ?"    " : "│   "), true);
+            }
         }
     }
 }
